193e39cc6e5ba4f6b497181779e310b9eac6ebc2
Genevieve Schultz <genschultz@gmail.com>
2023-12-20 15:00:58 -0800

    initial setup and player movement

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
